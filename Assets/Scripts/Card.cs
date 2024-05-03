a3ee3fde87f0da4f9c3a8231fd0a65afae346960
Hai3 <ndtmivn123@gmail.com>
2024-05-03 14:46:59 +0700

    card

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -185,6 +185,6 @@ public class Card : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHand
     private void OnDestroy()
     {
         if(cardVisual != null)
-        Destroy(cardVisual.gameObject);
+            Destroy(cardVisual.gameObject);
     }
 }
